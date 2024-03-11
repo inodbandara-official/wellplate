2171986757e30127f08b08c71b95fe6ffc52dba8
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2024-03-11 21:12:37 +0530

    Update signup.php

--- Frontend/pages/signup.php
+++ Frontend/pages/signup.php
@@ -53,42 +53,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="signup-container">
     <h2>Sign Up</h2>
     <form action="signup.php" method="post">
-        <div class="form-group">
+    <div class="form-group">
             <div class="input-box">
-                <input type="text" id="username" name="username" placeholder="Username" required>
+                <input type="text" id="name" placeholder="First Name" required>
                 <i class='bx bxs-user'></i>
             </div>
         </div>
         <div class="form-group">
             <div class="input-box">
-                <input type="email" id="email" name="email" placeholder="Email" required>
+                <input type="text" id="name" placeholder="Last Name" required>
+                <i class='bx bxs-user'></i>
+            </div>
+        </div>
+
+        <div class="form-group">
+        <div class="input-box">
+                <input type="number" id="age" placeholder="Age" required>
+                <i class='bx bxs-smile'></i>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="input-box">
+                <input type="tel" id="mobile-number" placeholder="Mobile Number" required>
+                <i class='bx bxs-phone'></i>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <div class="input-box">
+                <input type="email" id="email" placeholder="Email" required>
                 <i class='bx bxs-envelope' ></i>
             </div>
         </div>
+
         <div class="form-group">
             <div class="input-box">
-                <input type="password" id="password" name="password" placeholder="Password" required>
+                <input type="password" id="password" placeholder="Password" required>
                 <i class='bx bxs-lock-alt' ></i>
             </div>
-        </div>
-        <div class="form-group">
-            <div class="input-box">
-                <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm Password" required>
-                <i class='bx bxs-lock-alt' ></i>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="input-box">
-                <input type="text" id="allergies" name="allergies" placeholder="Any Allergies">
-                <i class='bx bxl-discourse'></i>
-            </div>
-        </div>
-        <div class="form-group">
-            <select id="primary-diet" name="primary-diet">
-                <option value="vegetarian">Vegetarian</option>
-                <option value="vegan">Vegan</option>
-            </select>
-            
         </div>
         <div class="form-group">
             <button type="submit">Sign Up</button>
