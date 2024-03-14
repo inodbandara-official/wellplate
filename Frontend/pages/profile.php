4ad7f6343e125a9add92b0d01f50f9daef52e65e
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2024-03-14 12:00:28 +0530

    Update profile.php
    Added the navbar and footer separately.

--- Frontend/pages/profile.php
+++ Frontend/pages/profile.php
@@ -58,64 +58,7 @@ if (isset($_SESSION['user_email'])) {
     </style>
 </head>
 <body>
-  <div class="sidebar">
-    <div class="logo_details">
-      <i class="bx xl-audible icon"></i>
-      <div class="logo_name">WELL PLATE</div>
-      <i class="bx bx-menu" id="btn"></i>
-    </div>
-    
-    <ul class="nav-list">
-      <li>
-        <a href="dashboard.php">
-          <i class="bx bx-grid-alt"></i>
-          <span class="link_name">Dashboard</span>
-        </a>
-        <span class="tooltip">Dashboard</span>
-      </li>
-      <li>
-        <a href="generateMealPlan.php">
-          <i class='bx bx-cog'></i>
-          <span class="link_name">Generate Meal Plan</span>
-        </a>
-        <span class="tooltip">Generate Meal Plan</span>
-      </li>
-      <li>
-        <a href="viewMealPlan.php">
-          <i class="bx bx-folder"></i>
-          <span class="link_name">View Meal Plan </span>
-        </a>
-        <span class="tooltip">View Meal Plan</span>
-      </li>
-      <li>
-        <a href="tracker.html">
-          <i class="bx bx-pie-chart-alt-2"></i>
-          <span class="link_name">Progress Tracker</span>
-        </a>
-        <span class="tooltip">Progress Tracker</span>
-      </li>
-      <li>
-        <a href="login.php">
-          <i class="bx bx-log-out"></i>
-          <span class="link_name">Log Out</span>
-        </a>
-        <span class="tooltip">Log Out</span>
-      </li>
-    
-      <li class="profile">
-        <div class="profile_details">
-          <a href="profile.php">
-          <img src="../assets/images/profile.jpg" alt="profile image">
-          <div class="profile_content">
-            <div class="name"><?php echo $userName; ?></div>
-            <div class="designation">Admin</div>
-          </div>
-        </a>
-        </div>
-        
-      </li>
-    </ul>
-  </div>
+<?php include '../attachedSections/navbar.php'; ?>
 
   <section class="home-section">
   <div class="profile-container">
@@ -133,20 +76,5 @@ if (isset($_SESSION['user_email'])) {
   <script src="../assets/js/navbar.js"></script>
   
 </body>
-<footer class="footer">
-  <div class="footer-content">
-    <div class="footer-logo">
-      <i class="bx xl-audible icon"></i>
-      <div class="logo_name">WELL PLATE</div>
-    </div>
-    <div class="footer-links">
-      <a href="#">Privacy Policy</a>
-      <a href="#">Terms of Service</a>
-      <a href="aboutus.html">About Us</a>
-    </div>
-  </div>
-  <div class="footer-bottom">
-    <p>&copy; 2024 Well Plate. All rights reserved.</p>
-  </div>
-</footer>
+<?php include '../attachedSections/footer.php'; ?>
 </html>
