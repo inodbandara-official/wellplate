63da80c396cde18775a878f1e0f8076ed1303161
sanduladissanayake <sanduladissanayaka@gmail.com>
2024-03-25 03:53:09 +0530

    Update premadeplan.php
    Added premade meal plan got from our model

--- Frontend/pages/premadeplan.php
+++ Frontend/pages/premadeplan.php
@@ -34,43 +34,137 @@
     <div class="meal-card">
         <h2>Sunday</h2>
         <img src="../assets/images/sunday.jpg" alt="Sunday Meal">
-        <p>Meal description goes here.</p>
+        <p>Breakfast:<br><br>
+  Lean Beef<br>
+  Oats<br>
+  Mutton Curry<br><br>
+
+Lunch:<br><br>
+  Chole Bhature<br>
+  Dal<br>
+  Radish<br><br>
+
+Dinner:<br><br>
+  Rajma Chawal<br>
+  Brinjal Moju<br>
+  Malai Kofta<br>
+</p>
     </div>
 
     <div class="meal-card">
         <h2>Monday</h2>
         <img src="../assets/images/monday.jpg" alt="Monday Meal">
-        <p>Meal description goes here.</p>
+        <p>Breakfast:<br><br>
+    Tabbouleh<br>
+    Almonds<br>
+    BBQ Pulled Pork<br><br>
+
+  Lunch:<br><br>
+    Coconut Milk<br>
+    Bell Peppers<br>
+    Celery<br><br>
+
+  Dinner:<br><br>
+    Black Beans<br>
+    Kale<br>
+    Laban<br>
+</p>
     </div>
 
     <div class="meal-card">
         <h2>Tuesday</h2>
         <img src="../assets/images/tuesday.jpg" alt="Tuesday Meal">
-        <p>Meal description goes here.</p>
+        <p>Breakfast:<br><br>
+    Grilled Chicken Salad<br>
+    Vegetable Korma<br>
+    Fattoush<br><br>
+
+  Lunch:<br><br>
+    Stir-Fried Tofu<br>
+    Chana Masala<br>
+    Cucumber<br><br>
+
+  Dinner:<br><br>
+    Wood Apple (Bel) Curry <br>
+    Turkey Chili<br>
+    Raita</p>
     </div>
 
     <div class="meal-card">
         <h2>Wednesday</h2>
         <img src="../assets/images/wednesday.jpg" alt="Wednesday Meal">
-        <p>Meal description goes here.</p>
+        <p>Breakfast:<br><br>
+    Oats<br>
+    Spaghetti Bolognese<br>
+    Veggie Burger<br><br>
+
+  Lunch:<br><br>
+    Salmon<br>
+    Fried Chicken<br>
+    Avocado<br><br>
+
+  Dinner:<br><br>
+    Black Beans<br>
+    Matar Paneer<br>
+    Chicken Curry</p>
     </div>
 
     <div class="meal-card">
         <h2>Thursday</h2>
         <img src="../assets/images/thursday.jpg" alt="Thursday Meal">
-        <p>Meal description goes here.</p>
+        <p>Breakfast:<br><br>
+    Quinoa<br>
+    Hummus<br>
+    Macaroni and Cheese<br><br>
+
+  Lunch:<br><br>
+    Broccoli<br>
+    Chicken Korma<br>
+    Spinach<br><br>
+
+  Dinner:<br><br>
+    Chicken Soup<br>
+    BBQ Ribs<br>
+    Tuna</p>
     </div>
 
     <div class="meal-card">
         <h2>Friday</h2>
         <img src="../assets/images/friday.jpg" alt="Friday Meal">
-        <p>Meal description goes here.</p>
+        <p>Breakfast:<br><br>
+    Quinoa<br>
+    Mango Juice<br>
+    Spinach Salad<br><br>
+
+  Lunch:<br><br>
+    Chicken Kebab<br>
+    Chicken Tikka<br>
+    Falafel<br><br>
+
+  Dinner:<br><br>
+    Quinoa<br>
+    Black Beans<br>
+    Cod</p>
     </div>
 
     <div class="meal-card">
         <h2>Saturday</h2>
         <img src="../assets/images/saturday.jpeg" alt="Saturday Meal">
-        <p>Meal description goes here.</p>
+        <p>Breakfast:<br><br>
+    Quorn<br>
+    Quinoa<br>
+    Za'atar Manakish<br><br>
+
+  Lunch:<br><br>
+    Bhindi Masala<br>
+    Cauliflower Rice<br>
+    Garlic<br><br>
+
+  Dinner:<br><br>
+    Fish Curry<br>
+    Chia Seeds<br>
+    Rajma Masala
+</p>
     </div>
     </section>
 
